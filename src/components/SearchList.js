321d177cc76ec035ecb64c238180038e1372a395
edcnogueira <edjan.nogueira14@gmail.com>
2022-01-09 23:00:46 -0300

    primeiro commit

---
+++ src/components/SearchList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Card from "./Card";
+
+export default function SearchList({ filteredPersons }) {
+  const filtered = filteredPersons.map(person => (
+    <Card key={person.id} person={person} />
+  ));
+  return <div>{filtered}</div>;
+}
